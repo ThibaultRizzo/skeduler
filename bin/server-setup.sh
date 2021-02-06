5d4b9badd1b9f9ca35218fa05fefe4bbb0094662
Thibault RIZZO <thibault.rizzo@outlook.fr>
2021-02-06 10:40:29 +0100

    Fix nginx config

--- bin/server-setup.sh
+++ bin/server-setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 sudo apt update
 
+# Install node latest
+sudo npm cache clean -f
+sudo npm install -g npm@latest
+sudo npm install -g n
+sudo n stable
+sudo reboot # Not sure if useful
+
 # Uninstall old versions of docker
 sudo apt-get remove docker docker-engine docker.io
 
